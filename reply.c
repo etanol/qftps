fb0ffcb67c970a638126867fe41dbe4a413b99b0
Isaac Jurado <diptongo@gmail.com>
2008-08-20 19:24:01 +0200

    Added missing file (again).

---
+++ reply.c
@@ -0,0 +1,42 @@
+/*
+ * User FTP Server,  Share folders over FTP without being root.
+ * Copyright (C) 2008  Isaac Jurado
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#include "uftps.h"
+#include <unistd.h>
+
+
+/*
+ * Send a reply to the peer over the control connection.
+ */
+void reply (const char *str, int len)
+{
+        int  b;
+
+        debug("Reply '%*s'", len - 2, str);
+
+        do {
+                b = write(SS.control_sk, str, len);
+                if (b == -1)
+                        fatal("Control channel output");
+
+                str += b;
+                len -= b;
+        } while (len > 0);
+}
+
