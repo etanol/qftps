6fedac63a1b462174ed34c2c03bd78be4f9f6e01
Isaac Jurado <diptongo@gmail.com>
2008-10-07 12:39:56 +0200

    Fixed Hasefroch RETR implementation.
    Testing revealed that TransmitFile returns FALSE with a WSA_IO_PENDING error
    code (like EAGAIN for errno).  It also showed that GetOverlappedResult was not
    the function to use, as the wait has to be performed on the socket, not the
    file.
    
    An example from the Wine test suite provided the hint to use
    WSAGetOverlappedResult for sockets.  Now it works as desired.

--- send_file-hase.c
+++ send_file-hase.c
@@ -33,7 +33,7 @@ void send_file (void)
         int         f, e;
         off_t       size, completed;
         HANDLE      fh;
-        DWORD       b;
+        DWORD       b, xx;
         OVERLAPPED  ovlp;
         BOOL        ok = TRUE;
 
@@ -57,17 +57,26 @@ void send_file (void)
 
         while (completed < size)
         {
-                /* Set the overlapped file offset, according to "completed" */
+                /*
+                 * Instead of seeking the file, the file offset is specified
+                 * using an OVERLAPPED structure.  This forces TrasnmitFile to
+                 * behave in overlapped mode (non-blocking).
+                 *
+                 * As blocking behaviour is desired and TransmitFile with an
+                 * offset returns immediately, an explicit wait is required.
+                 * The wait function to use is WSAGetOverlappedResult with the
+                 * wait flag enabled.
+                 */
                 memset(&ovlp, 0, sizeof(OVERLAPPED));
                 ovlp.OffsetHigh = (completed >> 32) & 0x0000FFFFFFFFLL;
                 ovlp.Offset     =  completed        & 0x0000FFFFFFFFLL;
                 b  = (size - completed > INT_MAX ? INT_MAX : size - completed);
 
                 ok = TransmitFile(SS.data_sk, fh, b, 0, &ovlp, NULL, 0);
-                if (!ok)
+                if (!ok && WSAGetLastError() != WSA_IO_PENDING)
                         break;
 
-                ok = GetOverlappedResult(fh, &ovlp, &b, TRUE);
+                ok = WSAGetOverlappedResult(SS.data_sk, &ovlp, &b, TRUE, &xx);
                 if (!ok)
                         break;
 
