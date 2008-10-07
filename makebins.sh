3d159c8fb97b61248e4ef9548ae513858870ce4a
Isaac Jurado <diptongo@gmail.com>
2008-10-07 17:04:13 +0200

    Added support for building universal binaries on OSX.

--- makebins.sh
+++ makebins.sh
@@ -8,38 +8,56 @@
 platform=${1-'linux'}
 targets='all debug'
 flavours='generic mmap'
+cflags=''
+ldflags=''
 exe=''
+
 version=`hg id -t | head -1`
 if [ -z "$version" ] || [ "$version" = 'tip' ]
 then
     version=`hg id -i | head -1`
+    if [ -z "$version" ]
+    then
+        version='XXX'  # Just in case
+    fi
 fi
 
 case $platform in
     linux) platform="linux_`uname -m`"
-           flavours='generic mmap linux' ;;
+           flavours='generic mmap linux'
+           ;;
     hase)  targets='hase dhase'
            flavours='generic mmap hase'
-           exe='.exe'                    ;;
+           exe='.exe'
+           ;;
+    mac)   cflags='-arch i386 -arch ppc -arch ppc64'
+           ldflags='-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk'
+           ;;
     *) ;;
 esac
 
 id="${version}_$platform"
 
-test -d uftps-$id || mkdir uftps-$id
+[ -d uftps-bin-$id  ] || mkdir uftps-bin-$id
 
-for flavour in $flavours
+for flav in $flavours
 do
-    make RETR=$flavour $targets
-    mv -f uftps$exe      uftps-$id/uftps-$flavour$exe
-    mv -f uftps.dbg$exe  uftps-$id/uftps-$flavour.dbg$exe
+    if [ -n "$cflags" ] || [ -n "$ldflags" ]
+    then
+        make EXTRA_CFLAGS="$cflags" EXTRA_LDFLAGS="$ldflags" RETR=$flav $targets
+    else
+        make RETR=$flav $targets
+    fi
+
+    mv -f uftps$exe     uftps-bin-$id/uftps-$flav$exe
+    mv -f uftps.dbg$exe uftps-bin-$id/uftps-$flav.dbg$exe
 done
 
 if [ $platform = hase ]
 then
-    zip -rm9 uftps-$id.zip uftps-$id
+    zip -rm9 uftps-bin-$id.zip uftps-bin-$id
 else
-    tar cvf - uftps-$id | gzip -vfc9 >uftps-$id.tar.gz
-    rm -rf uftps-$id
+    tar cvf - uftps-bin-$id | gzip -vfc9 >uftps-bin-$id.tar.gz
+    rm -rf uftps-bin-$id
 fi
 
